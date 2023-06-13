13bfa3b9864d1cb990cc9e3dae2f1135871a5d9e
TheLadsters <jeraldmags@gmail.com>
2023-06-13 16:14:41 +0800

    Changed Send Button styling

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -8,6 +8,7 @@
         <title>Laravel Chat Mockup</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <link href="css/welcome.css" rel="stylesheet" />
+        <link href="css/buttons.css" rel="stylesheet" />
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -40,8 +41,8 @@
                                         <i class='bx bx-paperclip bx-sm'></i>
                                     </a>
 
-                                    <button id="message-button" type="submit" class="btn btn-primary">
-                                        <i class='bx bx-send'></i> Send
+                                    <button id="message-button" type="submit" class="btn-submit">
+                                        Send
                                     </button>
                                 </div>
                             </div>
