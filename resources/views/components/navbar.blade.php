900ba5e87a555bd022a1ef0ac55ff7694ae0cb29
TheLadsters <jeraldmags@gmail.com>
2023-06-13 16:14:41 +0800

    Changed Send Button styling

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,9 +4,9 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                    <li class="nav-item active"><a class="nav-link" href="#!"><i class='bx bx-video bx-sm'></i></a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!"><i class='bx bxs-phone-call bx-sm' ></i></a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!"><i class='bx bxs-user-plus bx-sm'></i></a></li>
+                    <!-- <li class="nav-item active"><a class="nav-link" href="#!"><i class='bx bx-video bx-sm'></i></a></li> -->
+                    <!-- <li class="nav-item"><a class="nav-link" href="#!"><i class='bx bxs-phone-call bx-sm' ></i></a></li> -->
+                    <li class="nav-item"><a class="nav-link" href="#!"><i class='bx bx-info-circle bx-sm'></i></a></li>
                 </ul>
             </div>
         </div>
