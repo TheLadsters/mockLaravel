7f6f7f8e78e330a6953a8f9cceebdced28df12b7
TheLadsters <jeraldmags@gmail.com>
2023-06-14 10:01:52 +0800

    Placed submitted messages at right side of screen

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -14,43 +14,8 @@
          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-        <div class="d-flex" id="wrapper">
-            <!-- Page content wrapper-->
-            <!-- <x-sidebar />    -->
-            <div id="page-content-wrapper">
-                <!-- Top navigation-->
-                <x-navbar />
-                <!-- Page content-->
-                <div class="container-fluid">
-                    <div class="fixed-bottom mx-5 mb-3">
-                        <form>
-                            <div id="message-list" class="messages--div">
-                            </div>
+        @yield('main-chat')   
 
-                            <div class="mb-3">
-                                <input id="message-input" type="text" class="form-control" placeholder="Type a new message">
-                            </div>
-
-                            <div class="row">
-                                <div class="col-12 d-flex flex-row justify-content-end">
-                                    <a class="nav-link">
-                                        <i class='bx bx-smile bx-sm'></i>
-                                    </a>
-                                    
-                                    <a class="nav-link">
-                                        <i class='bx bx-paperclip bx-sm'></i>
-                                    </a>
-
-                                    <button id="message-button" type="submit" class="btn-submit">
-                                        Send
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="js/welcome.js"></script>
---
+++ resources/views/pages/mainScreen.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.welcome')
+
+@section('main-chat')
+<div class="d-flex" id="wrapper">
+    <!-- Page content wrapper-->
+    <!-- <x-sidebar />    -->
+    <div id="page-content-wrapper">
+        <!-- Top navigation-->
+        <x-navbar />
+        <!-- Page content-->
+        <div class="container-fluid">
+            <div class="fixed-bottom mx-5 mb-3">
+                <form>  
+                    <div class="messages--div row">
+                        <div class="col-6" id="reply-list">
+                        </div>
+
+                        <div class="col-6" id="message-list">
+
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <input id="message-input" type="text" class="form-control" placeholder="Type a new message">
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12 d-flex flex-row justify-content-end">
+                            <a class="nav-link">
+                                <i class='bx bx-smile bx-sm'></i>
+                            </a>
+                                    
+                            <a class="nav-link">
+                                <i class='bx bx-paperclip bx-sm'></i>
+                            </a>
+
+                            <button id="message-button" type="submit" class="btn-submit">
+                                Send
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
